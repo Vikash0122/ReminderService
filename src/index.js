34c0682c1b5e48761c69792048d7a75ea05bda23
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-27 15:01:07 +0530

    setup of SMTP using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service');
 
 const setUpAndStartServer = () => {
     const app = express();
@@ -8,6 +9,13 @@ const setUpAndStartServer = () => {
     app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT, () => {
         console.log(`Server started at port: ${PORT}`);
+
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'vikash01nitp@gmail.com',
+        //     'Testing Email 2',
+        //     'Hey vikash, how are you? This is just a test email.'
+        // );
     })
 }
 
